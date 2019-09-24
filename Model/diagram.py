e8a5af5f4e53180b4309f9aa03bd2c7b46e6376f
SimonJB07 <48108956+SimonJB07@users.noreply.github.com>
2019-09-24 15:46:07 +1200

    Delete diagram.py

--- Model/diagram.py
+++
@@ -1,9 +0,0 @@
-
-
-# The whole product
-class Diagram(object):
-    def __init__(self):
-        self.dict = {"diagram": {"Cla": [], "Rel": [], "Att": [], "Met": []},
-                     "diagram_1": {"Cla": [], "Rel": [], "Att": [], "Met": []},
-                     }
-
